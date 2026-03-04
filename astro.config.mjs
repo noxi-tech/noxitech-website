d020019072098072f3391f387bc39ae3ae8348ce
Majd Assad <majdassad123@gmail.com>
2026-03-04 18:01:46 -0500

    feat: Create initial website structure with new pages, components, global styles, assets, and `astro-icon` integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import netlify from '@astrojs/netlify';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [icon()]
 });
